d773eb9ff743c3ce4b57973165c689217d8f5e29
Ramkumar Venkataraman <ramkumar.venkataraman@transferwise.com>
2019-11-01 12:56:57 +0800

    Fix PMD issue - dont use System.exit

--- src/main/java/com/nqueens/solver/Application.java
+++ src/main/java/com/nqueens/solver/Application.java
@@ -7,8 +7,8 @@ public class Application {
 	public static void main(String[] args) {
 
 		if(args.length < 1) {
-			System.err.println("boardDimension argument is missing. \nUsage gradlew run -PboardDimension=<a number>");
-			System.exit(1);
+			String error = "boardDimension argument is missing. \nUsage gradlew run -PboardDimension=<a number>";
+			throw new IllegalArgumentException(error);
 		}
 
 		try {
@@ -17,8 +17,8 @@ public class Application {
 			if(b.solve(Arrays.asList(Constraint.NO_THREE_QUEENS_SAME_LINE))) b.print();
 			else System.err.println("No possible solution for boardDimension = " + n);
 		} catch(NumberFormatException e) {
-			System.err.println("boardDimension argument is not an integer. \nUsage gradlew run -PboardDimension=<a number>");
-			System.exit(1);
+			String error = "boardDimension argument is not an integer. \nUsage gradlew run -PboardDimension=<a number>";
+			throw new IllegalArgumentException(error, e);
 		}
 	}
 }
